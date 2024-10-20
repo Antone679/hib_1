2da631cc30df9fa61608ed1520e77f5ee09e96a3
Anton Avdei <antongru679@gmail.com>
2024-10-20 12:18:34 +0300

    DONE

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,51 +1,108 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "666777999A");
+        sessionFactory = new Configuration()
+                .addProperties(properties)
+                .addAnnotatedClass(Player.class)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+
+        try (Session session = sessionFactory.openSession()) {
+            NativeQuery<Player> query = session.createNativeQuery("SELECT * FROM player_db", Player.class);
+            query.setFirstResult(pageNumber * pageSize);
+            query.setMaxResults(pageSize);
+            return query.list();
+        }
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+
+        try (Session session = sessionFactory.openSession()) {
+            Query<Long> query = session.createNamedQuery("player_namedQueryCount", Long.class);
+            return Math.toIntExact(query.uniqueResult());
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(player);
+            transaction.commit();
+        }
+        return player;
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.update(player);
+            transaction.commit();
+        }
+
+        return player;
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+
+        try (Session session = sessionFactory.openSession()) {
+            return Optional.of(session.find(Player.class, id));
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.remove(player);
+            transaction.commit();
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        try {
+            com.mysql.cj.jdbc.AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sessionFactory.close();
     }
 }
